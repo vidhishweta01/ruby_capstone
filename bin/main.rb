743ce56a0558590c4076da17c64023c5b443538b
vidhishweta01 <vidhishweta01@gmail.com>
2021-02-26 20:52:56 +0530

    added method for identation error

--- bin/main.rb
+++ bin/main.rb
@@ -11,13 +11,20 @@ begin
   if k.end_with?('.rb') || !k.include?('.')
     f = File.open(str[0])
     content = f.readlines.map(&:chomp) # content is an array of file lines
-    puts content[0]
   else
     error_message << 'enter the file with extention .rb'.colorize(:light_red)
   end
 rescue StandardError => e
   error_message << "Check file name or path again\n".colorize(:light_red) + e.to_s.colorize(:red)
 end
+content.length.times do |i|
+  line = content[i]
+  if ch.trailing_spaces(line)
+    error_message << "line no.#{i} having trailing spaces at the end".colorize(:red)
+  end
+end
+ch.check_indentation(content)
+ch.for_usage(content)
 if error_message.empty?
   puts "#{'No offenses'.colorize(:green)} detected"
 else
--- lib/checks.rb
+++ lib/checks.rb
@@ -8,11 +8,12 @@ class ReadLine
 
   def detect_keyword(line)
     state = false
-    array = ['do','def','if','unless','begin','for']
+    array = ['do ', 'def ', 'unless ', 'elsif', 'if ', 'begin ', 'for ' 'end']
     array.each do |i|
       if line.include?(i)
+        keyword = i
         state = true
-        return [state, i]
+        return [state, keyword]
       end
     end
     [state]
@@ -24,7 +25,7 @@ class Checks < ReadLine
   def initialize(array)
     @error = array
   end
-  def trailing_spaes(line)
+  def trailing_spaces(line)
     state = false
     state = true if line.end_with?(' ')
     state
@@ -33,20 +34,83 @@ class Checks < ReadLine
   def count_keyword(content)
     count = 0
     arr = []
-    content.each do |i|
-      j = detect_keyword(i)
+    content.length.times do |i|
+      j = detect_keyword(content[i])
       if j[0]
         count += 1
-        arr << [j[1], content.index(i)]
+        arr << [j[1], i]
       end
     end
     [count, arr]
   end
 
-  def indentation_error(content)
+  def blocks(content)
     j = count_keyword(content)
-    k = j[1]
-    
+    array = []
+    keywords = j[1]
+    keywords.length.times do |i|
+      arr = keywords[i]
+      key = ['if','unless']
+      key.each do |val|
+        if arr[0] == val
+          line = content[arr[1]]
+          k = line.index(val) - 1
+          res = (0..k).reject{|n| line[n] ==' '}
+          array << i if !res.empty?
+        end 
+      end
+    end
+    keyword = []
+    array.length.times do |i|
+      keyword << keywords[array[i]]
+    end
+    keywords -= keyword
+    keywords
   end
 
+  def for_usage(content)
+    content.length.times do |i|
+      line = content[i] 
+      if line.include?('for')
+        @error << "line:#{i+1} use each or times instead of for".colorize(:red)
+      end
+    end
+  end
+  
+  def check_indentation(content)
+    msg = 'IndentationWidth: Use 2 spaces for indentation.'
+    cur_val = 0
+    indent_val = 0
+    arr = []
+    content.each_with_index do |str_val, indx|
+      strip_line = str_val.strip.split(' ')
+      exp_val = cur_val * 2
+      res_word = %w[class def if elsif until module unless begin case]
+  
+      next unless !str_val.strip.empty? || !strip_line.first.eql?('#')
+  
+      indent_val += 1 if res_word.include?(strip_line.first) || strip_line.include?('do')
+      indent_val -= 1 if str_val.strip == 'end'
+  
+      next if str_val.strip.empty?
+  
+      @error << indent_error(str_val, indx, exp_val, msg)
+      cur_val = indent_val
+    end
+    arr
+  end
+
+  private
+
+  def indent_error(str_val, indx, exp_val, msg)
+    strip_line = str_val.strip.split(' ')
+    emp = str_val.match(/^\s*\s*/)
+    end_chk = emp[0].size.eql?(exp_val.zero? ? 0 : exp_val - 2)
+  
+    if str_val.strip.eql?('end') || strip_line.first == 'elsif' || strip_line.first == 'when'
+      return("line:#{indx + 1} #{msg}".colorize(:red)) unless end_chk
+    elsif !emp[0].size.eql?(exp_val)
+      return("line:#{indx + 1} #{msg}".colorize(:red))
+    end
+  end
 end
