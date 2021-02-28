6406a3f4b43505557450492ed1810dd4d411fd40
vidhishweta01 <vidhishweta01@gmail.com>
2021-02-28 20:53:20 +0530

    added sample file

---
+++ lib/sample.rb
@@ -0,0 +1,16 @@
+def my_count(var = nil)
+  c = 0
+  if block_given?
+    my_each { |i| c += 1 if yield(i) }
+  elsif !var.nil?
+    if to_a.include? var
+      to_a.length.times do |i|
+        c += 1 if to_a[i] == var
+      end
+    end
+    c
+  else
+    c = size
+  end
+  c
+end
